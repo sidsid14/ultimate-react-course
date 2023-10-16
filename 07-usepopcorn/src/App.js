949c197831ed49a155e871c02062cd8886da98b9
Sudhanshu Tiwari <sudhanshu@Sudhanshus-MacBook-Air.local>
2023-10-16 18:21:36 +0530

    useRef to access dom elements

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import StarRating from "./StarRating";
 
 const average = (arr) =>
@@ -297,6 +297,11 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(function () {
+    inputEl.current.focus();
+  }, []);
   return (
     <input
       className="search"
@@ -304,6 +309,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
