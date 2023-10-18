7293dfff9082b71202bd772ade288a90dd2af352
Sudhanshu Tiwari <sudhanshu@Sudhanshus-MacBook-Air.local>
2023-10-18 23:25:40 +0530

    start a new quiz

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -4,6 +4,7 @@ import Main from "./Main";
 import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const initialState = {
   questions: [],
@@ -17,6 +18,8 @@ function reducer(state, action) {
       return { ...state, questions: action.payload, status: "ready" };
     case "dataFailed":
       return { ...state, questions: action.payload, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
     default:
       return new Error("Unknown action");
   }
@@ -38,7 +41,10 @@ export default function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question />}
       </Main>
     </div>
   );
