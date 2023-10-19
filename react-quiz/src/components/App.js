ca0392816342fb0746c37c28c004b0cb4dc330a1
Sudhanshu Tiwari <sudhanshu@Sudhanshus-MacBook-Air.local>
2023-10-19 09:25:26 +0530

    restart quiz

--- react-quiz/src/components/App.js
+++ react-quiz/src/components/App.js
@@ -50,6 +50,13 @@ function reducer(state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       };
+    case "restart":
+      return {
+        ...initialState,
+        questions: state.questions,
+        status: "ready",
+        highscore: state.highscore,
+      };
     default:
       return new Error("Unknown action");
   }
@@ -107,6 +114,7 @@ export default function App() {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
