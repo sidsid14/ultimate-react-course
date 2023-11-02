e35c1455042af27b63f4b8f9c2816344e384e24c
Sudhanshu Tiwari <sudhanshu@Sudhanshus-MacBook-Air.local>
2023-11-02 22:15:55 +0530

    styling

--- fast-react-pizza/src/features/order/CreateOrder.jsx
+++ fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
+import Button from "../../ui/Button";
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
+    str,
   );
 
 const fakeCart = [
@@ -45,13 +46,13 @@ function CreateOrder() {
       <Form method="POST">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input className="input" type="text" name="customer" required />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input className="input" type="tel" name="phone" required />
           </div>
           {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
@@ -59,7 +60,7 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input type="text" name="address" required />
+            <input className="input" type="text" name="address" required />
           </div>
         </div>
 
@@ -68,6 +69,7 @@ function CreateOrder() {
             type="checkbox"
             name="priority"
             id="priority"
+            className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
@@ -75,9 +77,9 @@ function CreateOrder() {
         </div>
         <input type="hidden" name="cart" value={JSON.stringify(cart)} />
         <div>
-          <button disabled={isSubmitting}>
+          <Button disabled={isSubmitting} className="">
             {isSubmitting ? "Placing Order..." : "Order now"}
-          </button>
+          </Button>
         </div>
       </Form>
     </div>
--- fast-react-pizza/src/features/user/Username.jsx
+++ fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Sudhanshu</div>;
+  return <div className="hidden text-sm font-semibold md:block">Sudhanshu</div>;
 }
 
 export default Username;
