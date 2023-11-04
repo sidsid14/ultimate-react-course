6f5b27ec4c25b8e977dbc718018ff0663191fdd0
Sudhanshu Tiwari <sudhanshu@Sudhanshus-MacBook-Air.local>
2023-11-04 18:03:27 +0530

    Reusable Button and Row component

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -3,6 +3,7 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const H1 = styled.h1`
   font-size: 32px;
@@ -13,14 +14,25 @@ function App() {
   return (
     <>
       <GlobalStyles />
-      <div>
-        <Heading as="h1">The Wild Oasis</Heading>
-        <Heading as="h2">Check in and out</Heading>
-        <Heading as="h3">Form</Heading>
-        <Button>Check In</Button>
-        <Button>Check Out</Button>
-        <Input type="number" placeholder="Number of guests" />
-      </div>
+      <Row>
+        <Row type="horizontal">
+          <Heading as="h1">The Wild Oasis</Heading>
+          <div>
+            <Heading as="h2">Check in and out</Heading>
+            <Button>Check In</Button>
+            <Button variation="secondary" size="small">
+              Check Out
+            </Button>
+          </div>
+        </Row>
+        <Row>
+          <Heading as="h3">Form</Heading>
+          <form>
+            <Input type="number" placeholder="Number of guests" />
+            <Input type="number" placeholder="Number of guests" />
+          </form>
+        </Row>
+      </Row>
     </>
   );
 }
