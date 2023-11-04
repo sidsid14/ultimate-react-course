5e739df048dfdd9ae4ed28b6b8d0b674fb9b286f
Sudhanshu Tiwari <sudhanshu@Sudhanshus-MacBook-Air.local>
2023-11-04 17:40:36 +0530

    styled component props and css function

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
 
 const H1 = styled.h1`
   font-size: 32px;
@@ -11,7 +14,12 @@ function App() {
     <>
       <GlobalStyles />
       <div>
-        <H1>Hello World</H1>
+        <Heading as="h1">The Wild Oasis</Heading>
+        <Heading as="h2">Check in and out</Heading>
+        <Heading as="h3">Form</Heading>
+        <Button>Check In</Button>
+        <Button>Check Out</Button>
+        <Input type="number" placeholder="Number of guests" />
       </div>
     </>
   );
