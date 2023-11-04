a7e5994ac827effb672c4908dddc0ef1ad50ae6c
Sudhanshu Tiwari <sudhanshu@Sudhanshus-MacBook-Air.local>
2023-11-04 16:21:51 +0530

    adding global styles

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,5 +1,20 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 32px;
+  font-weight: 600;
+  color: orangered;
+`;
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <div>
+        <H1>Hello World</H1>
+      </div>
+    </>
+  );
 }
 
 export default App;
