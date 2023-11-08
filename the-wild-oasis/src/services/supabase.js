210eee7c6b9b824022c385d6feba8ca5d9cfacbd
Sudhanshu Tiwari <sudhanshu@Sudhanshus-MacBook-Air.local>
2023-11-08 07:34:51 +0530

    connecting supabase with react app

---
+++ the-wild-oasis/src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://jhqzaumyqadizjdvbbol.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpocXphdW15cWFkaXpqZHZiYm9sIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkzMjA1NzIsImV4cCI6MjAxNDg5NjU3Mn0.OxYhvMKO1ws-m2pwcBY9RPRefcNEg61WNRHWSa0LUZI";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
