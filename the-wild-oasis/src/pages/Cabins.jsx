c6212fefaa0faf2c224dd25037f7b6abb7ed0416
Sudhanshu Tiwari <sudhanshu@Sudhanshus-MacBook-Air.local>
2023-11-10 17:09:19 +0530

    handling form and uploading file

--- the-wild-oasis/src/pages/Cabins.jsx
+++ the-wild-oasis/src/pages/Cabins.jsx
@@ -1,13 +1,12 @@
-import { useEffect } from "react";
+import { useState } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
 import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
-  useEffect(function () {
-    getCabins().then((data) => console.log(data));
-  }, []);
+  const [showForm, setShowForm] = useState(false);
 
   return (
     <>
@@ -17,6 +16,10 @@ function Cabins() {
       </Row>
 
       <CabinTable />
+      <Button onClick={() => setShowForm((show) => !show)}>
+        Add new Cabin
+      </Button>
+      {showForm && <CreateCabinForm />}
     </>
   );
 }
